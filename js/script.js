4d61902574d414976368e893f705879b5b87e052
Muhammet Komur <muhammet@dentsuachtung.com>
2022-11-03 10:40:43 +0100

    disable / enabling button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-let os = "Unkown"
+let os = "Unkown";
 
 if (navigator.appVersion.indexOf("Win") != -1) os = "Windows";
 if (navigator.appVersion.indexOf("Mac") != -1) os = "Mac OS";
-if (navigator.appVersion.indexOf("X11") != -1) os = "UNIX";
-if (navigator.appVersion.indexOf("Linux") != -1) os = "Linux";
 
 document.querySelector(".os").innerHTML = os;
+
+const nextbtn = document.querySelector("#nextbtn");
+
+if (os === "Mac OS") {
+    console.log("mac");
+    nextbtn.classList.add("disabledbtn");
+    nextbtn.disabled = true;
+
+} else if (os === "Windows") {
+    console.log("windows")
+    nextbtn.classList.remove("disabledbtn");
+    nextbtn.disabled = false;
+
+} else {
+    console.log("Other OS")
+    nextbtn.classList.add("disabledbtn");
+    nextbtn.disabled = true;
+
+}
